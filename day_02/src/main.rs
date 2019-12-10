a9a729e7580d93f2cb6ef42528e6c2e4759244a1
Jaakko Saarinen <pod@fea.st>
2019-12-10 20:40:20 +0200

    About to do something with our values

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,21 +1,43 @@
-extern crate regex;
+//extern crate regex;
 
-use regex::Regex;
+//use regex::Regex;
 use std::fs;
 
 
 fn main() {
     let contents = fs::read_to_string("data.txt").expect("Error reading input");
 
-    let mut input: Vec<u32> = Vec::new();
+    /*     
     let re = Regex::new("[0-9]+").unwrap();
-
 
     for n in re.captures_iter(&contents) {
         input.push((&n[0]).parse().unwrap());
+    } */
+
+    let mut input: Vec<u32> = Vec::new();
+    
+    for s in contents.split(',') {
+        input.push(s.trim().parse().unwrap());
     }
 
-    for n in input {
-        // Do things
+    for j in input {
+        match j {
+            1 => {
+
+            },
+            2 => {
+
+            },
+            99 => {
+                break;
+            },
+            _ => (),
+        }
     }
+}
+
+
+
+#[cfg(test)]
+mod tests {
 }
