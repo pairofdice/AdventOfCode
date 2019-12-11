fce6681acb8a1afd18a121b1d77c5a1eb0c6d52f
Jaakko Saarinen <pod@fea.st>
2019-12-11 21:21:17 +0200

    aww yis

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -14,26 +14,38 @@ fn main() {
         input.push((&n[0]).parse().unwrap());
     } */
 
-    let mut input: Vec<i32> = Vec::new();
-    let input_copy = input.clone();
+    let mut input: Vec<u32> = Vec::new();
     
     for s in contents.split(',') {
         input.push(s.trim().parse().unwrap());
     }
+    let input_copy = input.clone();
     
-    let input = run(input);
+    for noun in 0..100 {
+        for verb in 0..100 {
+            input = input_copy.clone();
+            
+            input[1] = noun;
+            input[2] = verb;
+            if run(input)[0] == 19690720 {
+                println!("noun: {}\t verb:{}", noun, verb);
+                println!("result?: {}{}", noun, verb);
+                break;
+            }
+        }
+    }
 
-
-    println!("{}", input[0]);
 }
 
-fn run(mut input: Vec<i32>) -> Vec<i32> {
+fn run(mut input: Vec<u32>) -> Vec<u32> {
     let mut code = input[0];
     let mut index = 0;
 
     while code != 99 {
         code = input[index];
-
+        if code > (input.len()-6) as u32{
+            continue;
+        }
         match code {
             1 => {
                 let i_a = input[index + 1] as usize;
