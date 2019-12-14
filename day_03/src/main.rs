e002cf7288c68a80d28c3733dc6c5bf80a9b9ba9
Jaakko Saarinen <pod@fea.st>
2019-12-14 22:45:47 +0200

    Babysteps

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -1,3 +1,26 @@
+use std::collections::HashSet;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("data.txt").expect("Error reading input");
+
+    let mut wires: Vec<(&str, i32)> = Vec::new();
+
+    
+    for line in contents.lines() {
+        for s in line.split(',') {
+            
+            let dir = &s.trim()[..1];
+            let len = &s.trim()[1..];
+            wires.push((dir, len.parse().unwrap()));
+        } 
+    }
+
+    for (dir, len) in wires {
+        println!("{}: {}", dir, len);
+    }
+    
+    let _wire: HashSet<(i32, i32)> = HashSet::new();
+
+
 }
