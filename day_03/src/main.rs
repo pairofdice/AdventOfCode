899b418305de8688ae3a09ecdce1689efbdf73e3
Jaakko Saarinen <pod@fea.st>
2019-12-15 07:35:35 +0200

    Wires are separate

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -4,20 +4,24 @@ use std::fs;
 fn main() {
     let contents = fs::read_to_string("data.txt").expect("Error reading input");
 
-    let mut wires: Vec<(&str, i32)> = Vec::new();
+    let mut wires: Vec<Vec<(&str, i32)>> = Vec::new();
 
     
     for line in contents.lines() {
+        let mut wire: Vec<(&str, i32)> = Vec::new();
         for s in line.split(',') {
             
             let dir = &s.trim()[..1];
             let len = &s.trim()[1..];
-            wires.push((dir, len.parse().unwrap()));
+            wire.push((dir, len.parse().unwrap()));
         } 
+        wires.push(wire);
     }
 
-    for (dir, len) in wires {
-        println!("{}: {}", dir, len);
+    for wire in wires {
+        for (dir, len) in wire {
+            println!("{}: {}", dir, len);
+        }
     }
     
     let _wire: HashSet<(i32, i32)> = HashSet::new();
