ebb6060c128f44bbce06920ef62d142c94e9fb06
Jaakko Saarinen <pod@fea.st>
2021-12-01 11:00:55 +0200

    Reading a file

--- aoc_2021/day_01_a.rs
+++ aoc_2021/day_01_a.rs
@@ -1,13 +1,8 @@
 use std::fs;
 
 fn main() {
-<<<<<<< HEAD:aoc_2021/day_1_a.rs
     let data = fs::read_to_string("day01a.txt").expect("Error reading file");
     for i in data.lines() {
-        println!(i);
+        println!("{}", i);
     }
 }
-=======
-    println!("Hello {}!", "World");
-}
->>>>>>> cc3893a7ee813e64f0864a2096b3b957dad627ed:aoc_2021/day_01_a.rs
