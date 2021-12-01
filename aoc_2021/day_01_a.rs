943f6a6f85ce21be95880812df6bf92295221acf
Jaakko Saarinen <pod@fea.st>
2021-12-01 12:09:54 +0200

    refactoring

--- aoc_2021/day_01_a.rs
+++ aoc_2021/day_01_a.rs
@@ -1,13 +1,18 @@
 use std::fs;
 
-fn main() {
-    let mut counter = 0;
+fn read_into_ints() -> Vec<i64> {
     let data = fs::read_to_string("01a.txt").expect("Error reading file");
     let mut darray: Vec<i64> = Vec::new();
     for line in data.lines() {
         let n = line.trim().parse().unwrap();
         darray.push(n);
     }
+    darray
+}
+
+fn main() {
+    let darray = read_into_ints();
+    let mut counter = 0;
     for (i, n) in darray.iter().enumerate().skip(1) {
         if darray[i - 1] < *n {
             counter += 1;
