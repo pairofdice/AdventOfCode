f36f9402a9ab17a8c3e652c7f7bfe003a44bc97e
Jaakko Saarinen <pod@fea.st>
2021-12-01 11:38:04 +0200

    day 1 a, gold star

--- aoc_2021/day_01_a.rs
+++ aoc_2021/day_01_a.rs
@@ -1,8 +1,20 @@
 use std::fs;
 
 fn main() {
-    let data = fs::read_to_string("day01a.txt").expect("Error reading file");
-    for i in data.lines() {
-        println!("{}", i);
+    let mut counter = 0;
+    let data = fs::read_to_string("01a.txt").expect("Error reading file");
+    let mut darray: Vec<i64> = Vec::new();
+    for line in data.lines() {
+        let n = line.trim().parse().unwrap();
+        darray.push(n);
     }
+    for (i, n) in darray.iter().enumerate() {
+        if i == 0 {
+            continue;
+        }
+        if darray[i - 1] < *n {
+            counter += 1;
+        }
+    }
+    println!("{}", counter);
 }
