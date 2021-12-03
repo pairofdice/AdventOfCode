f508cfd4bd31d2ea9c2a11008e3b1776f53a3c30
Jaakko Saarinen <pod@fea.st>
2021-12-03 12:37:55 +0200

    Final v6

--- aoc_2021/day_03.rs
+++ aoc_2021/day_03.rs
@@ -1,15 +1,5 @@
-/*
-Each bit in the gamma rate can be determined by finding the most common bit in the corresponding position of all numbers in the diagnostic report.
-
-The epsilon rate is calculated in a similar way; rather than use the most common bit, the least common bit from each position is used.
-*/
-
-fn read() -> Vec<i32> {
-    include_str!("03.txt").lines().map(|line| i32::from_str_radix(line, 2).unwrap()).collect()
-}
-
 fn main() {
-    let darray = read();
+    let darray:Vec<i32> = include_str!("03.txt").lines().map(|line| i32::from_str_radix(line, 2).unwrap()).collect();
     let mut omega = [0; 12];
     for n in darray {
         let mut mystery = 2048;
