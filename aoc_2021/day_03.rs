f8375df4b466b4a276621614fdcae5381c322860
Jaakko Saarinen <pod@fea.st>
2021-12-03 12:41:42 +0200

    Final v7

--- aoc_2021/day_03.rs
+++ aoc_2021/day_03.rs
@@ -12,9 +12,7 @@ fn main() {
             mystery /= 2;
         }
     }
-    let gamma = i32::from_str_radix(&omega.map(|n: i32| (if n > 0 { 1 } else { 0 }).to_string()).join(""), 2).unwrap();
-
+    let gamma = i32::from_str_radix(&omega.map(|n| (if n > 0 { 1 } else { 0 }).to_string()).join(""), 2).unwrap();
     let epsilon = !gamma & 4095;
-    
     println!("Part1, gamma: {} epsilon: {} multiplied {}", gamma, epsilon, gamma * epsilon);
 }
