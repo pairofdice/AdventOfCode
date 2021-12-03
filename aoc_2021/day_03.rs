f5b6a50ac5ffd8e9070c98d03b4ac36e9b0100d5
Jaakko Saarinen <pod@fea.st>
2021-12-03 11:59:50 +0200

    hold on now

---
+++ aoc_2021/day_03.rs
@@ -0,0 +1,31 @@
+/*
+Each bit in the gamma rate can be determined by finding the most common bit in the corresponding position of all numbers in the diagnostic report.
+
+The epsilon rate is calculated in a similar way; rather than use the most common bit, the least common bit from each position is used.
+*/
+
+fn read() -> Vec<i32> {
+    include_str!("03.txt")
+        .lines()
+        .map(|line| i32::from_str_radix(line, 2).unwrap())
+        .collect()
+}
+
+fn main() {
+    let darray = read();
+    let mut omega = [0; 12];
+    for n in darray {
+        let mut mystery = 2048;
+        for x in 0..12 {
+            if (n & mystery) >= mystery {
+                omega[x] += 1;
+            } else {
+                omega[x] -= 1;
+            }
+            mystery /= 2;
+        }
+    }
+    let gamma =  i32::from_str_radix(&omega.map(|n: i32| (if n > 0 {1} else {0}).to_string()).join(""), 2).unwrap();
+    let epsilon =  i32::from_str_radix(&omega.map(|n: i32| (if n > 0 {0} else {1}).to_string()).join(""), 2).unwrap();
+    println!("Part1, gamma: {} epsilon: {} multiplied {}", gamma, epsilon, gamma * epsilon);
+}
