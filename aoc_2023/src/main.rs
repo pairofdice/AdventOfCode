a813f72cb7e5ed4d40699da700d6bde4c680ab60
Jaakko Saarinen <jaakko.saarinen@fastmail.com>
2023-12-02 17:48:22 +0200

    Day 02 done!

--- aoc_2023/src/main.rs
+++ aoc_2023/src/main.rs
@@ -1,9 +1,68 @@
+#[allow(dead_code)]
 fn main() {
     println!("Hello, world!");
-    day01();
+    // day01();
+    day02();
 }
 
+fn day02() {
+    let input: &str = include_str!("../day02.txt");
+    let mut total = 0;
+    let mut power_total = 0;
+    let contains_red = 12;
+    let contains_green = 13;
+    let contains_blue = 14;
 
+    for line in input.lines() {
+        let game: Vec<&str> = line.split(' ').collect();
+        let id: i32 = game.get(1).unwrap().strip_suffix(':').unwrap().parse().unwrap();
+        // print!("{id}: ");
+        // println!("{game:?}");
+
+        let mut g_max_g = 0;
+        let mut g_max_b = 0;
+        let mut g_max_r = 0;
+
+        for i in (2..game.len()).step_by(2) {
+            let count: i32 = game.get(i).unwrap().parse().unwrap();
+            // print!("{count}");
+            match game.get(i + 1) {
+                None => {}
+                Some(color) => {
+                    match color.trim_end_matches([',',';']) {
+                        "red" => {
+                            if count > g_max_r {
+                                g_max_r = count;
+                            }
+                        },
+                        "green" => {
+                            if count > g_max_g {
+                                g_max_g = count;
+                            }
+                        },
+                        "blue" => {
+                            if count > g_max_b {
+                                g_max_b = count;
+                            }
+                        },
+                        _ => {}
+                    }
+                }
+            }
+          //  print!("{}", game.get(i).unwrap());
+        }
+        println!("{}", g_max_r * g_max_g * g_max_b);
+        power_total += g_max_r * g_max_g * g_max_b;
+
+        if g_max_r <= contains_red && g_max_g <= contains_green  && g_max_b <= contains_blue {
+            // println!("VALID!");
+            total += id;
+        }
+    }
+    println!("Total: {total}, Power sum: {power_total}");
+}
+
+#[allow(dead_code)]
 fn day01() {
     let mut total: i64 = 0;
     let input = include_str!("../day01.txt");
@@ -11,7 +70,7 @@ fn day01() {
     for line in input.lines() {
         let (a, b) = find_wordnum_indx(line);
         // print!("{} - {:?}, {:?}", line, a.unwrap_or_default(), b.unwrap_or_default());
-    print!("{} -", line);
+        // print!("{} -", line);
         let mut c;
         let c1;
         if let Some(i) = line.find(char::is_numeric) {
@@ -28,11 +87,12 @@ fn day01() {
             c2 = resolve_second_num(0, -1, b.unwrap_or((-1, 0)));
         }
         let num = format!("{}{}", c1, c2).parse::<i64>().unwrap();
-        println!(" {}", num);
+        // println!(" {}", num);
         total += num;
     }
-    println!("Total: {}", total);
+    println!("Day 01 total: {}", total);
 }
+
 fn find_wordnum_indx(s: &str) -> (Option<(i64, i64)>, Option<(i64, i64)>) {
     let nums = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
 
@@ -77,5 +137,18 @@ fn resolve_second_num(digit: i64, digit_index: i64, wordnum:(i64, i64)) -> i64 {
         digit
     } else {
         wordnum.1
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_day02() {
+        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
     }
 }
