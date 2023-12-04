9a3c4aaa86d28129a86a73f74594ce21cfb79cf6
Jaakko Saarinen <jaakko.saarinen@fastmail.com>
2023-12-04 12:39:11 +0200

    Day3 part 1 done

--- aoc_2023/src/main.rs
+++ aoc_2023/src/main.rs
@@ -2,9 +2,102 @@
 fn main() {
     println!("Hello, world!");
     // day01();
-    day02();
+    // day02();
+    day03()
 }
 
+fn day03() {
+    let input: Vec<&str> = include_str!("../day03.txt").lines().collect();
+//     let input: Vec<&str> = "467..114..
+// ...*......
+// ..35..633.
+// ......#...
+// 617*......
+// .....+.58.
+// ..592.....
+// ......755.
+// ...$.*....
+// .664.598..".lines().collect();
+    let mut total = 0;
+    for line_i in 0..input.len() {
+        if let Some(line) = input.get(line_i) {
+            let char_indx = line.char_indices();
+            let mut isnum = false;
+            let mut num = 0;
+            let mut numlen = 0;
+            let mut index = 0;
+            for (i, c) in char_indx
+            {
+                if c.is_numeric() {
+                    if let Some(cc) = c.to_digit(10) {
+                        num *= 10;
+                        num += cc;
+                        numlen += 1;
+                        if !isnum {
+                            index = i;
+                        }
+                    }
+                    isnum = true;
+                } else {
+                    if isnum {
+                        // we've read a number, we know everything about it
+                        // now we need to check neighbours
+                        let mut found_symbol: bool = false;
+                        let mut index_end = index + numlen + 1;
+                        if index_end > line.len() {
+                            index_end = line.len();
+                        }
+                        // print!("{index}:{num}-{numlen} ");
+                        if index == 0 {
+                            index = 1;
+                        }
+                        if line_i >= 1 {
+                            if let Some(prevline) = input.get(line_i - 1) {
+                                let slice = &prevline[(index - 1)..index_end];
+                                if find_symbols(slice) {
+                                    found_symbol = true;
+                                }
+                                // println!("{slice}");
+                            }
+                        }
+                        let slice = &line[(index - 1)..index_end];
+                        if find_symbols(slice) {
+                            found_symbol = true;
+                        }
+                        // println!("{slice} ");
+
+                        if let Some(nextline) = input.get(line_i + 1) {
+                            let slice = &nextline[(index - 1)..index_end];
+                            if find_symbols(slice) {
+                                found_symbol = true;
+                            }
+                        }
+
+                        if found_symbol {
+                            total += num;
+                            // println!("{num}");
+                        }
+                        num = 0;
+                        numlen = 0;
+                        isnum = false;
+                    }
+                }
+            }
+        }
+        println!();
+    }
+    println!("{total}");
+}
+
+fn find_symbols(s: &str) -> bool {
+    if s.contains(|s: char| !(s.is_numeric() || s == '.') ) {
+        return true;
+    }
+    false
+}
+
+
+#[allow(dead_code)]
 fn day02() {
     let input: &str = include_str!("../day02.txt");
     let mut total = 0;
@@ -29,32 +122,32 @@ fn day02() {
             match game.get(i + 1) {
                 None => {}
                 Some(color) => {
-                    match color.trim_end_matches([',',';']) {
+                    match color.trim_end_matches([',', ';']) {
                         "red" => {
                             if count > g_max_r {
                                 g_max_r = count;
                             }
-                        },
+                        }
                         "green" => {
                             if count > g_max_g {
                                 g_max_g = count;
                             }
-                        },
+                        }
                         "blue" => {
                             if count > g_max_b {
                                 g_max_b = count;
                             }
-                        },
+                        }
                         _ => {}
                     }
                 }
             }
-          //  print!("{}", game.get(i).unwrap());
+            //  print!("{}", game.get(i).unwrap());
         }
         println!("{}", g_max_r * g_max_g * g_max_b);
         power_total += g_max_r * g_max_g * g_max_b;
 
-        if g_max_r <= contains_red && g_max_g <= contains_green  && g_max_b <= contains_blue {
+        if g_max_r <= contains_red && g_max_g <= contains_green && g_max_b <= contains_blue {
             // println!("VALID!");
             total += id;
         }
@@ -74,14 +167,14 @@ fn day01() {
         let mut c;
         let c1;
         if let Some(i) = line.find(char::is_numeric) {
-            c = line.get(i..i+1).unwrap();
+            c = line.get(i..i + 1).unwrap();
             c1 = resolve_first_num(c.parse().unwrap(), i as i64, a.unwrap_or((666, 0)));
         } else {
             c1 = resolve_first_num(0, 666, a.unwrap_or((666, 0)));
         }
         let c2;
-        if let Some(j) = line.rfind(char::is_numeric){
-            c = line.get(j..j+1).unwrap();
+        if let Some(j) = line.rfind(char::is_numeric) {
+            c = line.get(j..j + 1).unwrap();
             c2 = resolve_second_num(c.parse().unwrap(), j as i64, b.unwrap_or((-1, 0)));
         } else {
             c2 = resolve_second_num(0, -1, b.unwrap_or((-1, 0)));
@@ -109,7 +202,7 @@ fn find_wordnum_indx(s: &str) -> (Option<(i64, i64)>, Option<(i64, i64)>) {
                     ai = j;
                     result_a = Some((j as i64, nums_index as i64 + 1));
                 }
-            },
+            }
             None => (),
         }
         match s.rfind(num) {
@@ -118,21 +211,22 @@ fn find_wordnum_indx(s: &str) -> (Option<(i64, i64)>, Option<(i64, i64)>) {
                     bi = j;
                     result_b = Some((j as i64, nums_index as i64 + 1));
                 }
-            },
+            }
             None => (),
         }
     }
     (result_a, result_b)
 }
 
-fn resolve_first_num(digit: i64, digit_index: i64, wordnum:(i64, i64)) -> i64 {
+fn resolve_first_num(digit: i64, digit_index: i64, wordnum: (i64, i64)) -> i64 {
     if digit_index < wordnum.0 as i64 {
         digit
     } else {
         wordnum.1
     }
 }
-fn resolve_second_num(digit: i64, digit_index: i64, wordnum:(i64, i64)) -> i64 {
+
+fn resolve_second_num(digit: i64, digit_index: i64, wordnum: (i64, i64)) -> i64 {
     if digit_index > wordnum.0 as i64 {
         digit
     } else {
@@ -142,7 +236,6 @@ fn resolve_second_num(digit: i64, digit_index: i64, wordnum:(i64, i64)) -> i64 {
 
 #[cfg(test)]
 mod tests {
-
     #[test]
     fn test_day02() {
         let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
